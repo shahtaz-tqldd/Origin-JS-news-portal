d19e0c68583ad21a8766bbadc20059214c6f05a3
Shahtaz Rahman <shahtazrahman17@gmail.com>
2022-09-03 02:12:35 +0600

    card added

--- js/app.js
+++ js/app.js
@@ -44,9 +44,31 @@ const displayNews = news =>{
     news.forEach(singleNews =>{
         const divNews = document.createElement('div')
         divNews.innerHTML = `
-            <h1>${singleNews.title}</h1>
+            <div class="card mb-3">
+            <div class="row g-0">
+                <div class="col-md-4">
+                <img src="${singleNews.image_url}" class="img-fluid rounded-start" alt="...">
+                </div>
+                <div class="col-md-8">
+                <div class="card-body">
+                    <h5 class="card-title">${singleNews.title}</h5>
+                    <p class="card-text">${singleNews.details.slice(0,400)+'...'}</p>
+                    <div class="d-flex">
+                        <img src="${singleNews.author.img}" class="author-img">
+                        <div>
+                            <p>${singleNews.author.name}</p>
+                            <p>${singleNews.author.published_date}</p>
+                        </div>
+                    </div>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                </div>
+                </div>
+            </div>
+            </div>
         `
         newsContainer.appendChild(divNews)
 
     })
 }
+
+btnCategory('05')
