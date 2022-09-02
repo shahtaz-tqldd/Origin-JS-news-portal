7f6548338f6b54b71c71f318518c24d72c1206b2
Shahtaz Rahman <shahtazrahman17@gmail.com>
2022-09-02 22:59:48 +0600

    news total number & news display added

--- js/app.js
+++ js/app.js
@@ -1,15 +1,18 @@
+/*
+1. Load Category
+*/ 
 const loadCategory =async() =>{
-    const url = 'https://openapi.programming-hero.com/api/news/categories'
+    const url = 'https://openapi.programming-hero.com/api/news/categories';
 
-    const res = await fetch(url)
-    const data = await res.json()
+    const res = await fetch(url);
+    const data = await res.json();
     displayCategory(data.data.news_category);
 }
 
 const displayCategory = categories =>{
-    const categoryContainer = document.getElementById('news-category')
+    const categoryContainer = document.getElementById('news-category');
     categories.forEach(category => {
-        console.log(category)
+        // console.log(category)
         const li = document.createElement('li')
         li.classList.add('category-list')
         li.innerHTML = `<span onclick="btnCategory('${category.category_id}')">${category.category_name}</span>`
@@ -20,7 +23,30 @@ const displayCategory = categories =>{
 
 loadCategory();
 
-const btnCategory =id =>{
-    const url = `https://openapi.programming-hero.com/api/news/category/${id}`
-    console.log(url)
+/*
+2. Load News from the Category
+*/
+const btnCategory =async(id)=>{
+    const url = `https://openapi.programming-hero.com/api/news/category/${id}`;
+    
+    const res = await fetch(url);
+    const data = await res.json();
+    displayNews(data.data);
+}
+
+const displayNews = news =>{
+    console.log(news)
+    const newsTotalContainer = document.getElementById('total-news-item')
+    newsTotalContainer.innerText = news.length? news.length : 'No News'
+
+    const newsContainer = document.getElementById('news-container')
+    newsContainer.innerHTML =  ``
+    news.forEach(singleNews =>{
+        const divNews = document.createElement('div')
+        divNews.innerHTML = `
+            <h1>${singleNews.title}</h1>
+        `
+        newsContainer.appendChild(divNews)
+
+    })
 }
